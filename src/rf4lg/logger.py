932077a8680b45785a8883d270876a74e88c24e4
Lassi Nousiainen <lassi_nou@hotmail.com>
2026-08-05 22:22:57 +0300

    Add logging module

--- src/rf4lg/logger.py
+++ src/rf4lg/logger.py
@@ -1,18 +1,37 @@
 """Logging setup for RF4 League Engine."""
 
 import logging
+import os
 
 
-def setup_logger(name: str = "rf4lg", level: int = logging.INFO) -> logging.Logger:
-    """Create and configure a logger for the package."""
+def get_logger(name: str) -> logging.Logger:
+    """Return a configured logger for RF4 League Engine.
+
+    The logger prints to the console and uses INFO level by default. Setting
+    the environment variable `RF4_DEBUG=1` enables DEBUG logging.
+
+    Args:
+        name: Logger name to configure.
+
+    Returns:
+        Configured logging.Logger instance.
+    """
     logger = logging.getLogger(name)
-    if not logger.handlers:
-        handler = logging.StreamHandler()
-        formatter = logging.Formatter(
-            fmt="%(asctime)s %(levelname)s [%(name)s] %(message)s",
-            datefmt="%Y-%m-%d %H:%M:%S",
-        )
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
-    logger.setLevel(level)
+    if logger.handlers:
+        logger.setLevel(_get_log_level())
+        return logger
+
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter("[%(levelname)s] %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(_get_log_level())
+    logger.propagate = False
     return logger
+
+
+def _get_log_level() -> int:
+    """Determine logging level from environment variables."""
+    if os.getenv("RF4_DEBUG") == "1":
+        return logging.DEBUG
+    return logging.INFO
